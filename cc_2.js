cc169adb94a5532fe3d5cbaccef2215c0299bf7c
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-01-26 14:43:41 -0500

    "Task 2 - Student Scores."

--- cc_2.js
+++ cc_2.js
@@ -2,6 +2,18 @@ let products = ["Brocolli","fries","whopper","Juice","Ice Cream"];
 products.push ("pie");
 console.log(products);
 let productsUpdated = products.pop();
-console.log(productsUpdated)
-console,log(products)
+console.log(productsUpdated);
+console,log(products);
 //task 1 - Store Inventory
+
+let scores = [75, 20, 80 , 95, 100];
+scores.splice (1,1, 50)
+console.log(scores)
+
+let totalscores = scores.reduce((sum, score) => sum + score, 0);
+let averagescores = totalscores / scores.length;
+
+console.log ("Updated array", scores);
+console.log("Average Scores =", averagescores);
+
+//Task 2 - student scores
